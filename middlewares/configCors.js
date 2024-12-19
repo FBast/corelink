e9e6f596d652a657b7d9e61ef55af2f3d9e2bdb2
Fred Bast <fred@ensi.lan>
2024-12-19 15:51:09 +0100

    Added deploy actions

--- middlewares/configCors.js
+++ middlewares/configCors.js
@@ -1,23 +1,23 @@
 ﻿import cors from 'cors';
 
-const corsOptions = {
-    origin: 'http://localhost:5173', // Origine spécifique autorisée
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Méthodes autorisées
-    allowedHeaders: ['Content-Type', 'Authorization', 'x-api-key'], // En-têtes autorisés
-    credentials: true // Maintient l'envoi des cookies si nécessaire
-};
-
 // const corsOptions = {
-//     origin: (origin, callback) => {
-//         const allowedDomains = process.env.ALLOWED_DOMAINS ? process.env.ALLOWED_DOMAINS.split(',') : [];
-//
-//         if (!origin || allowedDomains.includes(origin)) {
-//             callback(null, true);
-//         } else {
-//             callback(new Error('Accès non autorisé par CORS'));
-//         }
-//     },
-//     credentials: true
+//     origin: 'http://localhost:5173', // Origine spécifique autorisée
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Méthodes autorisées
+//     allowedHeaders: ['Content-Type', 'Authorization', 'x-api-key'], // En-têtes autorisés
+//     credentials: true // Maintient l'envoi des cookies si nécessaire
 // };
+
+const corsOptions = {
+    origin: (origin, callback) => {
+        const allowedDomains = process.env.ALLOWED_DOMAINS ? process.env.ALLOWED_DOMAINS.split(',') : [];
+
+        if (!origin || allowedDomains.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Accès non autorisé par CORS'));
+        }
+    },
+    credentials: true
+};
 
 export default cors(corsOptions);
